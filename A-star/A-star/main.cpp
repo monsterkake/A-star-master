75d3539e1a41cb7249ae2a7dfb1355739309b496
monsterkake <56121889+monsterkake@users.noreply.github.com>
2021-02-20 19:50:46 +0300

    external

---
+++ A-star/A-star/main.cpp
@@ -0,0 +1,69 @@
+#include "Map.h"
+#include "SFML\Graphics.hpp"
+#include "SFML\OpenGL.hpp"
+
+void algorithm(Map& map)
+{
+	bool done = false;
+	vector<Node*> path;
+	path.push_back(map.nodes[map.startPoint[0]][map.startPoint[1]]);
+
+	while (!done)
+	{
+
+		int bestNeighbour = -1;
+		for (int i = 0; i < (path[path.size() - 1]->neighbours.size()); i++)
+		{
+			if (path[path.size() - 1]->neighbours[i]->value > path[path.size() - 1]->value && !path[path.size() - 1]->neighbours[i]->isTested)
+			{
+				bestNeighbour = i;
+			}
+		}
+		if (bestNeighbour >= 0)
+			path.push_back(path[path.size() - 1]->neighbours[bestNeighbour]);
+		else
+		{
+			path[path.size() - 1]->isTested = true;
+			path.pop_back();
+		}
+
+		if (path.size() == 0)
+			path.push_back(map.nodes[map.startPoint[0]][map.startPoint[1]]);
+		else
+		{
+			if (path[path.size() - 1]->isFinish)
+			{
+				done = true;
+			}
+			else if (path[path.size() - 1]->value > map.nodes[map.finishPoint[0]][map.finishPoint[1]]->value)
+			{
+				path[path.size() - 1]->isTested = true;
+				path.pop_back();
+			}
+		}
+		/*
+		cout << "-------------------------------------------------------------------" << endl;
+		for (int i = 0; i < path.size(); i++)
+		{
+			cout << path[i]->value << "  ";
+		}
+		cout << "-------------------------------------------------------------------" << endl;
+		*/
+	}
+	for (int i = 0; i < path.size(); i++)
+	{
+		path[i]->isPathPart = true;
+	}
+}
+
+int main()
+{
+	sf::RenderWindow window;
+	window.create(sf::VideoMode(100, 100), "main");
+	Map map(10);
+	map.setValues();
+	map.show();
+	algorithm(map);
+	map.show();
+}
+
